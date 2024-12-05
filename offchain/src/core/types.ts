d91157101660435eebde2feb380d89234df3cfb3
hade <liveithade@gmail.com>
2024-12-05 22:57:26 +0700

    feat: init offchain

---
+++ offchain/src/core/types.ts
@@ -0,0 +1,7 @@
+export type Result<T> =
+  | { type: "ok"; data: T }
+  | { type: "error"; error: Error };
+
+export type Either<L, R> =
+  | { type: "left"; value: L }
+  | { type: "right"; value: R };
