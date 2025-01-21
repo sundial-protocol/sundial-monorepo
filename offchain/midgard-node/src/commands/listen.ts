d206328830f0e0709298e3e439ace6f422f9426f
keyanmaskoot <keyanmaskoot@gmail.com>
2025-01-21 14:15:41 +0100

    feat: setup express for receiving tx cbors

--- offchain/midgard-node/src/commands/listen.ts
+++ offchain/midgard-node/src/commands/listen.ts
@@ -1,11 +1,18 @@
-import { logInfo, logWarning } from "../utils.js";
+import { isHexString, logInfo, logWarning } from "../utils.js";
 import * as MPF from "@aiken-lang/merkle-patricia-forestry";
 import express from "express";
 
 export const listen = (port: number) => {
   const app = express();
   app.get("/", (req, res) => {
-    logWarning("TODO");
+    res.type("text/plain");
+    const txHex = req.query.tx;
+    const txIsString = typeof txHex === "string";
+    if (txIsString && isHexString(txHex)) {
+      res.send(`Transaction received: ${req.query.tx}`);
+    } else {
+      res.send("Please provide a valid transaction CBOR.");
+    }
   });
   app.listen(port, () => {});
   logInfo(`Server running at http://localhost:${port}`);
