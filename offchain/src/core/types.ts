062aa89a7f4bd305a64edd295084a89ba359e50a
keyanmaskoot <keyanmaskoot@gmail.com>
2025-01-21 17:07:01 +0100

    refactor: merge `midgard-offchain` branch

--- offchain/src/core/types.ts
+++
@@ -1,7 +0,0 @@
-export type Result<T> =
-  | { type: "ok"; data: T }
-  | { type: "error"; error: Error };
-
-export type Either<L, R> =
-  | { type: "left"; value: L }
-  | { type: "right"; value: R };
