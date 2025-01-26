c77d0565e663d23d6ba1362e1d22008f51b0a344
keyanmaskoot <keyanmaskoot@gmail.com>
2025-01-26 16:33:08 +0100

    fix: minor fix and formatting

--- offchain/midgard-sdk/src/endpoints/state-queue/fetch-latest-block.ts
+++ offchain/midgard-sdk/src/endpoints/state-queue/fetch-latest-block.ts
@@ -4,19 +4,20 @@ import {
   Data,
   LucidEvolution,
   PolicyId,
-  TxSignBuilder,
   UTxO,
 } from "@lucid-evolution/lucid";
-import {getSingleAssetApartFromAda} from "@/utils/helpers.js";
-import {NodeKey} from "@/types/contracts/linked-list/index.js";
-import {Datum} from "@/types/contracts/state-queue.js";
+import { getSingleAssetApartFromAda } from "@/utils/helpers.js";
+import { NodeKey } from "@/types/contracts/linked-list/index.js";
+import { Datum } from "@/types/contracts/state-queue.js";
 
 export type Config = {
   stateQueueAddress: Address;
   stateQueuePolicyID: PolicyId;
 };
 
-const getLinkFromBlockUTxO = (blockUTxO: UTxO): Either.Either<NodeKey, string> => {
+const getLinkFromBlockUTxO = (
+  blockUTxO: UTxO
+): Either.Either<NodeKey, string> => {
   const datumCBOR = blockUTxO.datum;
   if (datumCBOR) {
     try {
@@ -33,28 +34,35 @@ const getLinkFromBlockUTxO = (blockUTxO: UTxO): Either.Either<NodeKey, string> =
 export const fetchLatestBlock = (
   lucid: LucidEvolution,
   config: Config
-): Effect.Effect<[UTxO, NodeKey], string> =>
+): Effect.Effect<UTxO, string> =>
   Effect.gen(function* () {
     const allBlocks = yield* Effect.tryPromise({
       try: () => lucid.utxosAt(config.stateQueueAddress),
       catch: (_) => "Failed to fetch state queue UTxOs",
     });
-    const blockNFTsEithers: Either.Either<[UTxO, NodeKey], string>[] = allBlocks.map((u: UTxO) => {
-      const nfts = getSingleAssetApartFromAda(u.assets);
-      return Either.andThen(nfts, ([sym, _tn, qty]): Either.Either<[UTxO, NodeKey], string> => {
-        if (sym === config.stateQueuePolicyID && qty === 1n) {
-          const nodeKey = getLinkFromBlockUTxO(u);
-          return Either.map(nodeKey, (nk): [UTxO, NodeKey] => [u, nk]);
-        } else {
-          return Either.left("Unauthentic block");
-        }
+    const blockNFTsEithers: Either.Either<[UTxO, NodeKey], string>[] =
+      allBlocks.map((u: UTxO) => {
+        const nfts = getSingleAssetApartFromAda(u.assets);
+        return Either.andThen(
+          nfts,
+          ([sym, _tn, qty]): Either.Either<[UTxO, NodeKey], string> => {
+            if (sym === config.stateQueuePolicyID && qty === 1n) {
+              const nodeKey = getLinkFromBlockUTxO(u);
+              return Either.map(nodeKey, (nk): [UTxO, NodeKey] => [u, nk]);
+            } else {
+              return Either.left("Unauthentic block");
+            }
+          }
+        );
       });
-    });
-    const authenticBlockUTxOs = yield* Effect.filterMap(blockNFTsEithers, Option.some);
+    const authenticBlockUTxOs = yield* Effect.filterMap(
+      blockNFTsEithers,
+      Option.some
+    );
     const filtered = authenticBlockUTxOs.filter(([_u, nk]) => nk === "Empty");
     if (filtered.length === 1) {
-      return filtered[0];
+      return filtered[0][0];
     } else {
       return yield* Effect.fail("Latest block not found");
     }
-});
+  });
