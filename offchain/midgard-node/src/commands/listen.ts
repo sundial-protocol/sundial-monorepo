933c8082cb175ea9c0fe48502c1f9862e17c0a77
Cauterization <71918928+Cauterization@users.noreply.github.com>
2025-01-30 20:20:23 +0300

    feat: add archive database

--- offchain/midgard-node/src/commands/listen.ts
+++ offchain/midgard-node/src/commands/listen.ts
@@ -86,7 +86,7 @@ export const listen = (
   logInfo(`Server running at http://localhost:${port}`);
 };
 
-const monitorStateQueue = (lucid: LucidEvolution, pollingInterval: number) => {
+const monitorStateQueue = (lucid: LucidEvolution, db: sqlite3.Database, pollingInterval: number) => {
   let latestBlockOutRef: OutRef = { txHash: "", outputIndex: 0 };
   setInterval(async () => {
     const latestBlockOutRefRes = await fetchLatestBlock(lucid);
@@ -108,30 +108,12 @@ export const storeTx = (
   db: sqlite3.Database,
   tx: string
 ) => {
-  const query = `INSERT INTO mempool (tx_hash, tx_cbor) VALUES (?, ?)`;
   const txHash = lucid.fromTx(tx).toHash();
-  db.run(query, [txHash, tx], function (err) {
-    if (err) {
-      logAbort(`Error inserting transaction: ${err.message}`);
-    } else {
-      logInfo(`Transaction stored successfully with rowid ${this.lastID}`);
-    }
-  });
+  addToMempool(db, txHash, tx);
 };
 
 const submitBlock = async (lucid: LucidEvolution, latestBlock: UTxO) => {
   logWarning("submitBlock: TODO");
-};
-
-export const clearMempool = async (db: sqlite3.Database) => {
-  const query = `DELETE FROM mempool;`;
-  db.run(query, function (err) {
-    if (err) {
-      logAbort(`Error inserting transaction: ${err.message}`);
-    } else {
-      logInfo(`Transaction stored successfully with rowid ${this.lastID}`);
-    }
-  });
 };
 
 const monitorConfirmedState = (
@@ -141,20 +123,121 @@ const monitorConfirmedState = (
   logWarning("mergeOldestBlock: TODO");
 };
 
+export interface ArchiveTxRow {
+  tx_hash: string;
+  tx_cbor: string;
+}
+
+export const addToArchive = async (
+  db: sqlite3.Database,
+  blockHash: string,
+  txs: ArchiveTxRow[]
+) => {
+  const blockQuery = `
+    INSERT INTO archive_block (tx_hash, block_hash) VALUES
+    ${txs.map(tx => `(?, ?)`).join(', ')}
+  `;
+  const blockValues = txs.flatMap((tx) => [tx.tx_hash, blockHash]);
+  db.run(blockQuery, blockValues, function (err) {
+    if (err) {
+      logAbort(`Archive: error inserting block: ${err.message}`);
+    } else {
+      logInfo(`Archive: block stored with rowid ${this.lastID}`);
+    }
+  });
+  const txQuery = `
+    INSERT INTO archive_tx (tx_hash, tx_cbor) VALUES
+    ${txs.map(tx => `(?, ?)`).join(', ')}
+    `;
+  db.run(txQuery, txs.flatMap(tx => [tx.tx_hash, tx.tx_cbor]), function (err) {
+    if (err) {
+      logAbort(`Archive: error inserting txs: ${err.message}`);
+    } else {
+      logInfo(`Archive: ${txs.length} txs stored, last rowid: ${this.lastID}`);
+    }
+  });
+};
+
+export const clearArchive = async (db: sqlite3.Database) => {
+  const query = `DELETE FROM archive_block;`;
+  db.run(query, function (err) {
+    if (err) {
+      logAbort(`Archive: clearing error: ${err.message}`);
+    } else {
+      logInfo(`Archive: cleared`);
+    }
+  });
+};
+
+export interface MempoolRow {
+  tx_hash: string;
+  tx_cbor: string;
+}
+
+export const addToMempool = async (db : sqlite3.Database, txHash : string, tx_cbor : string) => {
+  const query = `INSERT INTO mempool (tx_hash, tx_cbor) VALUES (?, ?)`;
+  db.run(query, [txHash, tx_cbor], function (err) {
+    if (err) {
+      logAbort(`Mempool: error inserting tx to mempool: ${err.message}`);
+    } else {
+      logInfo(`Mempool: tx stored with rowid ${this.lastID}`);
+    }
+  });
+}
+
+export const retrieveMempool = async (db : sqlite3.Database) => {
+  const query = `SELECT * FROM mempool`;
+  const mempool = await new Promise<MempoolRow[]>((resolve, reject) => {
+    db.all(query, (err, rows: MempoolRow[]) => {
+      if (err) {
+        logAbort(`Mempool: retrieving error: ${err.message}`);
+        reject(err)
+      };
+      resolve(rows);
+    });
+  });
+  return mempool;
+};
+
+export const clearMempool = async (db: sqlite3.Database) => {
+  const query = `DELETE FROM mempool;`;
+  db.run(query, function (err) {
+    if (err) {
+      logAbort(`Mempool: clearing error: ${err.message}`);
+    } else {
+      logInfo(`Mempool: cleared`);
+    }
+  });
+};
+
 export async function initializeDb(dbFilePath: string) {
   const db = new sqlite3.Database(dbFilePath, (err) => {
     if (err) {
       logAbort(`Error opening database: ${err.message}`);
     } else {
-      logInfo("Connected to the SQLite database.");
+      logInfo("Connected to the SQLite database");
     }
   });
-  await db.exec(
-    `CREATE TABLE IF NOT EXISTS mempool (
+  await db.exec(`
+    PRAGMA foreign_keys = ON;
+    CREATE TABLE IF NOT EXISTS mempool (
       tx_hash BLOB NOT NULL UNIQUE,
-      tx_cbor BLOB NOT NULL,
+      tx_cbor BLOB NOT NULL UNIQUE,
       PRIMARY KEY (tx_hash)
-    );`
-  );
+    );
+    CREATE TABLE IF NOT EXISTS archive_block (
+      tx_hash BLOB NOT NULL UNIQUE,
+      block_hash BLOB NOT NULL,
+      PRIMARY KEY (tx_hash)
+    );
+    CREATE TABLE IF NOT EXISTS archive_tx (
+      tx_hash BLOB NOT NULL UNIQUE,
+      tx_cbor BLOB NOT NULL UNIQUE,
+      PRIMARY KEY (tx_hash)
+      FOREIGN KEY (tx_hash)
+        REFERENCES archive_block(tx_hash)
+        ON DELETE CASCADE
+    );
+  `);
   return db;
 }
--- offchain/midgard-node/test/listen.test.ts
+++ offchain/midgard-node/test/listen.test.ts
@@ -1,9 +1,10 @@
 import { describe, it, expect, beforeAll, afterAll } from "vitest";
 import sqlite3 from "sqlite3";
-import { clearMempool, initializeDb, storeTx } from "../src/commands/listen.js";
+import { addToArchive, ArchiveTxRow, clearArchive, clearMempool, initializeDb, retrieveMempool, storeTx } from "../src/commands/listen.js";
 import { CML, LucidEvolution } from "@lucid-evolution/lucid";
+import { logAbort } from "../src/utils.js";
 
-describe("mempool database", () => {
+describe("database", () => {
   let db: sqlite3.Database;
   const dbFilePath = ":memory:";
   const lucid = new MockLucid();
@@ -16,40 +17,93 @@ describe("mempool database", () => {
     db.close();
   });
 
-  const tx =
+  const tx1 =
     "84a300d9010281825820000000000000000000000000000000000000000000000000000000000000000000019582581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d605ceb8240667acbd2cb93d8efe342c0b9fa51f04678e3d5cddec94a881a05f5b9f082581d603b493979352930f3eccbd47ae0e12ef6dcea89326211cd80f87cf85a1b00000001dcd95d400200a100d901028182582015c6708ee2da48b2c46c5ab3e001ff636fa0b0e2a48b6676dffc8e36fe02c5985840599c8a11e9888ab60c10f78c708b55f6b542c6d70fa57775b26efe519dc9459604f2e1633601e2b9e448181e694278f3a89d8fea462dcfd772a6a9248e1f730df5f6";
+  const expectedRow1: ArchiveTxRow = {"tx_cbor" : tx1, "tx_hash": lucid.fromTx(tx1).toHash()};
 
-  it("should store a valid transaction", async () => {
-    storeTx(lucid as unknown as LucidEvolution, db, tx);
-
-    db.get(
-      "SELECT * FROM mempool WHERE tx_hash = ?",
-      [lucid.fromTx(tx).toHash()],
-      (err, row: MempoolRow) => {
-        expect(err).toBeNull();
-        expect(row.tx_cbor).toBe(tx);
-      }
-    );
+  it("should store transaction in the mempool", async () => {
+    storeTx(lucid as unknown as LucidEvolution, db, tx1);
+    const result = await retrieveMempool(db);
+    expect(result).toStrictEqual([expectedRow1]);
   });
 
   it("clears the mempool", async () => {
-    const initialRows = await new Promise<MempoolRow[]>((resolve, reject) => {
-      db.all("SELECT * FROM mempool", (err, rows: MempoolRow[]) => {
-        if (err) reject(err);
-        resolve(rows);
-      });
-    });
+    const initialRows = await retrieveMempool(db);
     expect(initialRows.length).toBe(1);
     await clearMempool(db);
-    const clearedRows = await new Promise<MempoolRow[]>((resolve, reject) => {
-      db.all("SELECT * FROM mempool", (err, rows: MempoolRow[]) => {
-        if (err) reject(err);
+    const result = await retrieveMempool(db);
+    expect(result.length).toBe(0);
+  });
+
+  const tx2 = "84a300d90102818258207351d824f3565c936f87d67c7352e0d34f13a51e6427bfad2a86e49bbae8a2dc14019582581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60902644a3189c282d9a54e4aeac15f03b16bef0fc17dd8aa6f9bc8b8f1a05f5b9f082581d60f2d5d0b7f8198330ba9410ffecdf45dcc67e774849bfbe8115a8cfcf1b0000000165a6d6800200a100d9010281825820f80e638fcfe3a3d60a785202efa9ff231b804db638896eb4a63bf6e0acceda405840567525cdfd1ee65e702c28901adbc16915a46f140dd91669c160b9b9095691205a26f7f95411ece1f2a9cd19901a6525a449256d4232eadf061db137967f0a09f5f6"
+  const expectedRow2: ArchiveTxRow = {"tx_cbor" : tx2, "tx_hash": lucid.fromTx(tx2).toHash()};
+  const blockHash = lucid.fromTx(tx1).toHash()
+
+  it("should store a block hash alongside with it's transactions in the archive", async () => {
+    await addToArchive(db, blockHash, [expectedRow1, expectedRow2]);
+    const result = await retrieveArchive(db);
+    expect(result).toStrictEqual(
+      [ {"block_hash": blockHash, ...expectedRow1}
+      , {"block_hash": blockHash, ...expectedRow2}
+      ]);
+  });
+
+  it("clears the archive", async () => {
+    const initialRows = await retrieveArchive(db);
+    expect(initialRows.length).toBe(2);
+    await clearArchive(db);
+    const resultBlocks = await new Promise<AchiveBlockRow[]>((resolve, reject) => {
+      db.all("SELECT * FROM archive_block", (err, rows: AchiveBlockRow[]) => {
+        if (err) {
+          logAbort(`Error retrieving archive: ${err.message}`);
+          reject(err)
+        };
         resolve(rows);
       });
     });
-    expect(clearedRows.length).toBe(0);
+    const resultTxs = await new Promise<ArchiveTxRow[]>((resolve, reject) => {
+      db.all("SELECT * FROM archive_tx", (err, rows: ArchiveTxRow[]) => {
+        if (err) {
+          logAbort(`Error retrieving archive: ${err.message}`);
+          reject(err)
+        };
+        resolve(rows);
+      });
+    });
+    expect(resultBlocks.length).toBe(0);
+    expect(resultTxs.length).toBe(0);
   });
+
 });
+
+interface AchiveBlockRow {
+  block_hash: string;
+  tx_hash: string;
+}
+interface ArchiveRow {
+  block_hash: string;
+  tx_hash: string;
+  tx_cbor: string;
+}
+
+const retrieveArchive = async (db : sqlite3.Database) => {
+  const query = `
+    SELECT archive_block.block_hash,
+           archive_block.tx_hash,
+           archive_tx.tx_cbor
+    FROM archive_block
+    JOIN archive_tx ON archive_block.tx_hash = archive_tx.tx_hash;`;
+  const result = await new Promise<ArchiveRow[]>((resolve, reject) => {
+    db.all(query, (err, rows: ArchiveRow[]) => {
+      if (err) {
+        logAbort(`Error retrieving archive: ${err.message}`);
+        reject(err)
+      };
+      resolve(rows);
+    });
+  });
+  return result;
+};
 
 class MockLucid {
   fromTx(tx: string) {
@@ -60,7 +114,3 @@ class MockLucid {
   }
 }
 
-interface MempoolRow {
-  tx_hash: string;
-  tx_cbor: string;
-}
