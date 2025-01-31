217b9003c98896549a607c07a7bb2a18f47d7084
keyanmaskoot <keyanmaskoot@gmail.com>
2025-01-31 22:54:12 +0100

    feat: first draft of merge endpoint

--- offchain/midgard-sdk/src/endpoints/state-queue/merge-to-confirmed-state.ts
+++ offchain/midgard-sdk/src/endpoints/state-queue/merge-to-confirmed-state.ts
@@ -1,17 +1,32 @@
 import { makeReturn } from "@/core.js";
 import { LucidEvolution, TxSignBuilder } from "@lucid-evolution/lucid";
-import { StateQueue, ActiveOperators } from "@/tx-builder/index.js";
+import { StateQueue } from "@/tx-builder/index.js";
 import { Effect } from "effect";
 import { errorToString } from "@/utils/common.js";
 import { FetchConfig } from "@/types/state-queue.js";
 
-export type MergeParams = {};
-
 export const mergeToConfirmedStateProgram = (
   lucid: LucidEvolution,
-  fetchConfig: FetchConfig,
-  params: MergeParams
+  fetchConfig: FetchConfig
 ): Effect.Effect<TxSignBuilder, string> =>
   Effect.gen(function* () {
-    return yield* Effect.fail("TODO");
+    const completedTx = yield* StateQueue.mergeTxBuilder(lucid, fetchConfig);
+    return yield* Effect.tryPromise({
+      try: () => completedTx.complete(),
+      catch: errorToString,
+    });
   });
+
+/**
+ * Builds completed tx for merging the first block in queue to be merged into
+ * the confirmed state.
+ *
+ * @param lucid - The `LucidEvolution` API object.
+ * @param fetchConfig - Configuration values required to know where to look for which NFT.
+ * @returns A promise that resolves to a `TxSignBuilder` instance.
+ */
+export const mergeToConfirmedState = (
+  lucid: LucidEvolution,
+  fetchConfig: FetchConfig
+): Promise<TxSignBuilder> =>
+  makeReturn(mergeToConfirmedStateProgram(lucid, fetchConfig)).unsafeRun();
