f0ca23baafdf4188d836f0a73b6a3832207a2f33
Georgii Gerasev <george30032002@gmail.com>
2025-02-20 17:08:03 +0200

    Genesis state draft

--- offchain/midgard-sdk/src/tx-builder/state-queue/init.ts
+++ offchain/midgard-sdk/src/tx-builder/state-queue/init.ts
@@ -1,8 +1,11 @@
-import { LucidEvolution, TxBuilder, Assets, PolicyId, toUnit } from "@lucid-evolution/lucid";
+import { ConfirmedState } from "@/types/contracts/ledger-state.js";
+import { NodeDatum } from "@/types/contracts/linked-list/index.js";
+import { LucidEvolution, TxBuilder, Assets, PolicyId, toUnit, Address, Data } from "@lucid-evolution/lucid";
 import { Effect } from "effect";
 
 export type InitParams = {
-  policyId : PolicyId
+    policyId : PolicyId
+  , address : Address
 };
 
 /**
@@ -17,10 +20,25 @@ export const initTxBuilder = (
   params: InitParams
 ): Effect.Effect <TxBuilder, Error> => {
   const tx = lucid.newTx();
-  const assets : Assets ={
+  const assets : Assets = {
     [toUnit(params.policyId, "Init")]: 1n,
    }
 
+  const confirmedState: ConfirmedState =  {
+      headerHash: ""
+    , prevHeaderHash: ""
+    , utxoRoot: ""
+    , startTime : 0n
+    , endTime : 0n
+    , protocolVersion: 0n
+  }
+  const datum : NodeDatum = {
+    key: { Key: { key: "" } },
+    next: { Key: { key: "" } },
+    data: Data.Object(confirmedState)
+  }
+
   tx.mintAssets(assets)
+  tx.pay.ToAddressWithData(params.address, datum, assets)
   return Effect.succeed(tx)
 };
