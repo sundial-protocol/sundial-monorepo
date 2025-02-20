185aee78b1387deacf3e87387b516f81c07a5a0e
Georgii Gerasev <george30032002@gmail.com>
2025-02-20 22:27:43 +0200

    initTxBuilder with proper types

--- demo/midgard-sdk/src/tx-builder/state-queue/init.ts
+++ demo/midgard-sdk/src/tx-builder/state-queue/init.ts
@@ -1,6 +1,13 @@
-import { LucidEvolution, TxBuilder } from "@lucid-evolution/lucid";
+import { ConfirmedState } from "@/types/contracts/ledger-state.js";
+import { NodeDatum } from "@/types/contracts/linked-list/index.js";
+import { LucidEvolution, TxBuilder, PolicyId, Address, Assets, toUnit, Data, OutputDatum, CBORHex } from "@lucid-evolution/lucid";
+import { Effect } from "effect";
 
-export type InitParams = {};
+
+export type InitParams = {
+    policyId : PolicyId
+  , address : Address
+};
 
 /**
  * Init
@@ -11,8 +18,34 @@ export type InitParams = {};
  */
 export const initTxBuilder = (
   lucid: LucidEvolution,
-  params: InitParams,
-): TxBuilder => {
+  params: InitParams
+): Effect.Effect <TxBuilder, Error> => {
   const tx = lucid.newTx();
-  return tx;
+  const assets : Assets = {
+    [toUnit(params.policyId, "Init")]: 1n,
+   }
+
+  const confirmedState: ConfirmedState =  {
+      headerHash: "00".repeat(28)
+    , prevHeaderHash: "00".repeat(28)
+    , utxoRoot: "00".repeat(32)
+    , startTime : BigInt(Date.now())
+    , endTime : BigInt(Date.now())
+    , protocolVersion: 0n
+  }
+  const datum : NodeDatum = {
+      key: "Empty"
+    , next: "Empty"
+    , data: Data.castTo(confirmedState, ConfirmedState)
+  }
+
+  const outputDatum : OutputDatum = {
+      kind: "inline"
+    , value: Data.to(datum, NodeDatum)
+  }
+
+  tx.mintAssets(assets)
+  tx.pay.ToAddressWithData(params.address, outputDatum, assets)
+  return Effect.succeed(tx)
 };
+
