474b89935b00f9ce53e503f8282c3f862f84b8cc
Georgii Gerasev <george30032002@gmail.com>
2025-02-21 20:33:50 +0200

    fix: proper address and network in the stateQueueInit

--- demo/midgard-node/src/transactions/state-queue/init.ts
+++ demo/midgard-node/src/transactions/state-queue/init.ts
@@ -11,12 +11,17 @@ import * as Blueprint from "../../../../always-succeeds/plutus.json";
 
 export const stateQueueInit = (lucid: LucidEvolution) =>
   Effect.gen(function* () {
+    const spendingScript: Script = {
+      type: "PlutusV3",
+      script: Blueprint.default.validators[1].compiledCode,
+    };
     const mintingScript: Script = {
       type: "PlutusV3",
       script: Blueprint.default.validators[0].compiledCode,
     };
+    const networkType = lucid.config().network ?? "Preprod"
     const initParams: SDK.Types.InitParams = {
-      address: validatorToAddress("Custom", mintingScript),
+      address: validatorToAddress(networkType, spendingScript),
       policyId: Blueprint.default.validators[0].hash,
       stateQueueMintingScript: mintingScript,
     };
