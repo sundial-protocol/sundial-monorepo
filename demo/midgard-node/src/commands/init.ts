c14a47a8ce4f401afba7cc65c600b5e5a2ad6be6
hade <liveithade@gmail.com>
2025-02-22 09:14:52 +0700

    fix: add init node script

--- demo/midgard-node/src/commands/init.ts
+++ demo/midgard-node/src/commands/init.ts
@@ -2,8 +2,7 @@ import { Effect } from "effect";
 import { stateQueueInit } from "@/transactions/state-queue/init.js";
 import { User } from "@/config.js";
 
-export const init =
-  Effect.gen(function* () {
-    const { user } = yield* User;
-    stateQueueInit(user);
-  });
+export const init = Effect.gen(function* () {
+  const { user } = yield* User;
+  stateQueueInit(user);
+});
