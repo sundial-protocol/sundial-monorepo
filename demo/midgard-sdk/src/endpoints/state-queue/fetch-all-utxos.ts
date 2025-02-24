b8ae710523ce2905f2035f1e78c9e9f746d4c15a
keyanmaskoot <keyanmaskoot@gmail.com>
2025-02-24 11:01:05 +0100

    feat: fetch endpoint for whole state queue list

---
+++ demo/midgard-sdk/src/endpoints/state-queue/fetch-all-utxos.ts
@@ -0,0 +1,35 @@
+import { LucidEvolution, UTxO } from "@lucid-evolution/lucid";
+import { FetchConfig } from "@/types/state-queue.js";
+import { Effect } from "effect";
+import { getSingleAssetApartFromAda, utxosAtByNFTPolicyId } from "@/utils/common.js";
+import { makeReturn } from "@/core.js";
+
+export const fetchAllAuthenticUTxOsProgram = (
+  lucid: LucidEvolution,
+  config: FetchConfig
+): Effect.Effect<{utxo: UTxO, assetName: string}[], Error> => Effect.gen(function* () {
+  const allUTxOs = yield* utxosAtByNFTPolicyId(
+    lucid,
+    config.stateQueueAddress,
+    config.stateQueuePolicyId
+  );
+  return yield* Effect.allSuccesses(allUTxOs.map((u: UTxO) => {
+    const singleAssetProgram = getSingleAssetApartFromAda(u.assets);
+    return Effect.map(singleAssetProgram, ([_sym, tn, _qty]) => {
+      // Quantity of 1 and policy ID are already checked.
+      return {utxo: u, assetName: tn};
+    });
+  }));
+});
+
+/**
+ * Attempts fetching the whole state queue linked list.
+ *
+ * @param lucid - The `LucidEvolution` API object.
+ * @param config - Configuration values required to know where to look for which NFT.
+ * @returns {UTxO[]} - All the authentic node UTxOs.
+ */
+export const fetchAllAuthenticUTxOs = (
+  lucid: LucidEvolution,
+  config: FetchConfig
+) => makeReturn(fetchAllAuthenticUTxOsProgram(lucid, config)).unsafeRun();
