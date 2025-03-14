10f73a476f17940793704378cac2abbcab93ac0e
keyanmaskoot <keyanmaskoot@gmail.com>
2025-03-14 14:53:14 +0100

    feat: add dedicated tree computation fns
    For UTxOs and transactions.

--- demo/midgard-sdk/src/utils/mpt.ts
+++ demo/midgard-sdk/src/utils/mpt.ts
@@ -1,5 +1,48 @@
 import { Trie } from "@aiken-lang/merkle-patricia-forestry";
 import { Effect } from "effect";
+import { UTxO, utxoToTransactionOutput, Data } from "@lucid-evolution/lucid";
+import { OutputReference } from "@/tx-builder/common.js";
+
+export const mptFromUTxOs = (utxos: UTxO[]): Effect.Effect<Trie, Error> =>
+  Effect.gen(function* () {
+    const data = utxos.map((utxo) => {
+      const outRef: OutputReference = {
+        outputIndex: BigInt(utxo.outputIndex),
+        txHash: { hash: utxo.txHash },
+      };
+      const cmlOutput = utxoToTransactionOutput(utxo);
+      return {
+        key: Data.to(outRef, OutputReference),
+        value: cmlOutput.to_cbor_hex(),
+      };
+    });
+
+    const trie = yield* Effect.tryPromise({
+      try: () => Trie.fromList(data),
+      catch: (e) => new Error(`${e}`),
+    });
+
+    return trie;
+  });
+
+export const mptFromTxs = (
+  txs: { txHash: string; txCbor: string }[],
+): Effect.Effect<Trie, Error> =>
+  Effect.gen(function* () {
+    const data = txs.map(({ txHash, txCbor }) => {
+      return {
+        key: txHash,
+        value: txCbor,
+      };
+    });
+
+    const trie = yield* Effect.tryPromise({
+      try: () => Trie.fromList(data),
+      catch: (e) => new Error(`${e}`),
+    });
+
+    return trie;
+  });
 
 /**
  * Create a Merkle Patricia Trie (MPT) from a list of items.
