6f1e3a8bbb302732353ed07419d6cad0f0a9b48e
keyanmaskoot <keyanmaskoot@gmail.com>
2025-04-22 01:08:46 +0200

    feat: extend `db` abstraction

--- demo/midgard-node/src/workers/db.ts
+++ demo/midgard-node/src/workers/db.ts
@@ -1,17 +1,22 @@
 import { NodeConfig } from "@/config.js";
-import { LatestLedgerCloneDB, LatestLedgerDB } from "@/database/index.js";
+import { LatestLedgerCloneDB, MempoolDB, UtilsDB } from "@/database/index.js";
 import { BatchDBOp, DB } from "@ethereumjs/util";
 import { Effect, pipe } from "effect";
 import pg from "pg";
+import { fromHex } from "@lucid-evolution/lucid";
 
-export class PostgresDB<TKey extends Uint8Array, TValue extends Uint8Array>
+export class PostgresDB<TKey extends string, TValue extends Uint8Array>
   implements DB<TKey, TValue>
 {
   _pool: pg.Pool | undefined;
   _database: Effect.Effect<pg.Pool, Error, NodeConfig>;
+  _tableName: string;
+  _referenceTableName: string | undefined;
 
-  constructor() {
-    this._pool = undefined;
+  constructor(tableName: string, referenceTableName?: string, pool?: pg.Pool) {
+    this._tableName = tableName;
+    this._referenceTableName = referenceTableName;
+    this._pool = pool;
     this._database = Effect.gen(function* () {
       const nodeConfig = yield* NodeConfig;
       return nodeConfig.DB_CONN;
@@ -19,23 +24,40 @@ export class PostgresDB<TKey extends Uint8Array, TValue extends Uint8Array>
   }
 
   async open() {
-    const pool = await Effect.runPromise(
-      pipe(this._database, Effect.provide(NodeConfig.layer))
-    );
-    this._pool = pool;
-    await pool.query(`
-INSERT INTO ${LatestLedgerCloneDB.tableName}
-SELECT * FROM ${LatestLedgerDB.tableName}`);
+    if (!this._pool) {
+      const pool = await Effect.runPromise(
+        pipe(this._database, Effect.provide(NodeConfig.layer))
+      );
+      this._pool = pool;
+      await pool.query(`
+CREATE TABLE IF NOT EXISTS ${this._tableName} (
+  key BYTEA NOT NULL,
+  value BYTEA NOT NULL,
+  PRIMARY KEY (key)
+);`);
+    }
+    if (this._referenceTableName) {
+      try {
+        await this._pool.query(`BEGIN`);
+        await this.clearReference();
+        await this._pool.query(`
+INSERT INTO ${this._tableName}
+SELECT * FROM ${this._referenceTableName}`);
+        await this._pool.query(`COMMIT`);
+      } catch (e) {
+        await this._pool.query(`ROLLBACK`);
+        throw e;
+      }
+    }
   }
 
   async get(key: TKey): Promise<TValue | undefined> {
     if (!this._pool) {
       throw new Error("Database not open");
     } else {
-      const query =
-        `SELECT tx_out_cbor FROM ${LatestLedgerCloneDB.tableName} WHERE tx_in_cbor = $1`;
+      const query = `SELECT value FROM ${this._tableName} WHERE key = $1`;
       try {
-        const result = await this._pool.query(query, [key]);
+        const result = await this._pool.query(query, [fromHex(key)]);
         if (result.rows.length > 0) {
           return result.rows[0].tx_cbor;
         } else {
@@ -51,9 +73,8 @@ SELECT * FROM ${LatestLedgerDB.tableName}`);
     if (!this._pool) {
       throw new Error("Database not open");
     } else {
-      const query =
-        `INSERT INTO ${LatestLedgerCloneDB.tableName} (tx_in_cbor, tx_out_cbor) VALUES ($1, $2)`;
-      await this._pool.query(query, [key, val]);
+      const query = `INSERT INTO ${LatestLedgerCloneDB.tableName} (key, value) VALUES ($1, $2)`;
+      await this._pool.query(query, [fromHex(key), val]);
     }
   }
 
@@ -61,8 +82,8 @@ SELECT * FROM ${LatestLedgerDB.tableName}`);
     if (!this._pool) {
       throw new Error("Database not open");
     } else {
-      const query = `DELETE FROM ${LatestLedgerCloneDB.tableName} WHERE tx_in_cbor = $1`;
-      await this._pool.query(query, [key]);
+      const query = `DELETE FROM ${LatestLedgerCloneDB.tableName} WHERE key = $1`;
+      await this._pool.query(query, [fromHex(key)]);
     }
   }
 
@@ -89,7 +110,61 @@ SELECT * FROM ${LatestLedgerDB.tableName}`);
     }
   }
 
+  async getAll(): Promise<{ key: Uint8Array; value: Uint8Array }[]> {
+    if (!this._pool) {
+      throw new Error("Database not open");
+    } else {
+      const result = await this._pool.query(`SELECT * FROM ${this._tableName}`);
+      return result.rows.map((row) => ({
+        key: row.key,
+        value: row.value,
+      }));
+    }
+  }
+
+  async clear(): Promise<void> {
+    if (!this._pool) {
+      throw new Error("Database not open");
+    } else {
+      await UtilsDB.clearTable(this._pool, this._tableName);
+    }
+  }
+
+  async clearReference(): Promise<void> {
+    if (!this._pool) {
+      throw new Error("Database not open");
+    } else if (this._referenceTableName) {
+      await UtilsDB.clearTable(this._pool, this._referenceTableName);
+    } else {
+      throw new Error("No reference tables set");
+    }
+  }
+
+  async transferToReference(): Promise<void> {
+    if (!this._pool) {
+      throw new Error("Database not open");
+    } else if (this._referenceTableName) {
+      try {
+        await this._pool.query(`BEGIN`);
+        await this.clearReference();
+        await this._pool.query(`
+INSERT INTO ${this._referenceTableName}
+SELECT * FROM ${this._tableName}`);
+        await this._pool.query(`COMMIT`);
+      } catch (e) {
+        await this._pool.query(`ROLLBACK`);
+        throw e;
+      }
+    } else {
+      throw new Error("No reference tables set");
+    }
+  }
+
   shallowCopy(): DB<TKey, TValue> {
-    return new PostgresDB();
+    return new PostgresDB(
+      this._tableName,
+      this._referenceTableName,
+      this._pool
+    );
   }
 }
