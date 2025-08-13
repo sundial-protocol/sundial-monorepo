07b6c6056ad4ddc4178fa1324fec539cec7b7bae
keyanmaskoot <keyanmaskoot@gmail.com>
2025-08-13 12:03:09 +0200

    fix: processed mempool db utils

--- demo/midgard-node/src/database/processedMempool.ts
+++ demo/midgard-node/src/database/processedMempool.ts
@@ -1,31 +1,25 @@
 import { Effect } from "effect";
-import {
-  KVPair,
-  clearTable,
-  insertKeyValue,
-  insertKeyValues,
-  retrieveKeyValues,
-  retrieveValue,
-  retrieveValues,
-} from "./utils.js";
+import * as Tx from "@/database/utils/tx.js";
+import { clearTable } from "@/database/utils/common.js";
 import { Database } from "@/services/database.js";
 
 export const tableName = "processed_mempool";
 
-export const insert = (tx: KVPair): Effect.Effect<void, Error, Database> =>
-  insertKeyValue(tableName, tx);
+export const insertTx = (
+  tx: Tx.Entry,
+): Effect.Effect<void, Error, Database> => Tx.insertEntry(tableName, tx);
 
 export const insertTxs = (
-  txs: KVPair[],
-): Effect.Effect<void, Error, Database> => insertKeyValues(tableName, txs);
+  txs: Tx.Entry[],
+): Effect.Effect<void, Error, Database> => Tx.insertEntries(tableName, txs);
 
-export const retrieve = () => retrieveKeyValues(tableName);
+export const retrieve = () => Tx.retrieveEntries(tableName);
 
 export const retrieveTxCborByHash = (txHash: Buffer) =>
-  retrieveValue(tableName, txHash);
+  Tx.retrieveValue(tableName, txHash);
 
 export const retrieveTxCborsByHashes = (
   txHashes: Buffer[] | readonly Buffer[],
-) => retrieveValues(tableName, txHashes);
+) => Tx.retrieveValues(tableName, txHashes);
 
 export const clear = () => clearTable(tableName);
